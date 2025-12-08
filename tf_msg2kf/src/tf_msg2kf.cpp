430363162fc78b5cdadc1e66c90bbda0de36a1c0
Zhao-Qihao <3070614305@qq.com>
2025-12-09 02:29:59 +0800

    update tf and fusion msg

--- tf_msg2kf/src/tf_msg2kf.cpp
+++ tf_msg2kf/src/tf_msg2kf.cpp
@@ -3,8 +3,8 @@
 #include <autoware_msgs/DetectedObjectArray.h>
 #include <autoware_msgs/DetectedObject.h>
 
-#include <kf_msgs/LidarObstacles.h>
-#include <kf_msgs/LidarObstacle.h>
+#include <kf_msgs/FusionObstacles.h>
+#include <kf_msgs/FusionObstacle.h>
 #include <kf_msgs/TrafficLights.h>
 #include <kf_msgs/TrafficLight.h>
 
@@ -18,6 +18,8 @@
 #include <unordered_map>
 #include <array>
 #include <cstdint>
+#include <tf/transform_listener.h>
+#include <geometry_msgs/PoseStamped.h>
 
 class TfMsg2Kf
 {
@@ -47,9 +49,11 @@ public:
     pnh_.param<int>("max_lost_frames", max_lost_frames_, 30);
 
     used_ids_.fill(false);
+    pnh_.param<std::string>("target_frame", target_frame_,
+                            std::string("base_link"));
 
     sub_ = nh_.subscribe(input_topic, 1, &TfMsg2Kf::objectsCallback, this);
-    pub_array_ = nh_.advertise<kf_msgs::LidarObstacles>(output_array_topic, 1);
+    pub_array_ = nh_.advertise<kf_msgs::FusionObstacles>(output_array_topic, 1);
 
     tl_sub_ = nh_.subscribe(tl_input_topic, 1, &TfMsg2Kf::trafficLightsCallback, this);
     tl_pub_ = nh_.advertise<kf_msgs::TrafficLights>(tl_output_topic, 1);
@@ -64,6 +68,8 @@ private:
   ros::NodeHandle pnh_;
   ros::Subscriber sub_;
   ros::Publisher pub_array_;
+  tf::TransformListener tf_listener_;
+  std::string target_frame_;
 
   ros::Subscriber tl_sub_;
   ros::Publisher tl_pub_;
@@ -85,28 +91,80 @@ private:
   {
     ++frame_counter_;
 
-    kf_msgs::LidarObstacles out;
-    out.header = msg->header;
-    out.obstacles.reserve(msg->objects.size());
+    autoware_msgs::DetectedObjectArray transformed = *msg;
+
+    const std::string& source_frame = msg->header.frame_id;
+    bool use_tf = true;
+
+    if (source_frame.empty())
+    {
+      ROS_WARN_THROTTLE(1.0, "objectsCallback: empty frame_id, skip TF transform");
+      use_tf = false;
+    }
+    else
+    {
+      try
+      {
+        // rslidar -> base_link（target_frame_）
+        tf_listener_.waitForTransform(
+            target_frame_, source_frame,
+            msg->header.stamp, ros::Duration(0.1));
+      }
+      catch (tf::TransformException& ex)
+      {
+        ROS_WARN_THROTTLE(1.0,
+                          "objectsCallback: waitForTransform %s -> %s failed: %s; using original poses",
+                          source_frame.c_str(), target_frame_.c_str(), ex.what());
+        use_tf = false;
+      }
+    }
+
+    if (use_tf)
+    {
+      for (size_t i = 0; i < transformed.objects.size(); ++i)
+      {
+        geometry_msgs::PoseStamped in_pose, out_pose;
+        in_pose.header = msg->header;          // frame_id = rslidar
+        in_pose.pose   = msg->objects[i].pose; // 原始 pose
+
+        try
+        {
+          tf_listener_.transformPose(target_frame_, in_pose, out_pose);
+          transformed.objects[i].pose = out_pose.pose;
+        }
+        catch (tf::TransformException& ex)
+        {
+          ROS_WARN_THROTTLE(1.0,
+                            "objectsCallback: transformPose %s -> %s failed: %s; keep original pose for object %zu",
+                            source_frame.c_str(), target_frame_.c_str(), ex.what(), i);
+        }
+      }
+      // 把 header 中的坐标系也更新为目标坐标系
+      transformed.header.frame_id = target_frame_;
+    }
+
+    kf_msgs::FusionObstacles out;
+    out.header = transformed.header;                 // 现在是 base_link
+    out.obstacles.reserve(transformed.objects.size());
 
     ROS_INFO_STREAM("objectsCallback: frame=" << frame_counter_
-                    << ", received objects size=" << msg->objects.size());
+                    << ", received objects size=" << transformed.objects.size());
 
-    for (const auto& obj : msg->objects)
+    for (const auto& obj : transformed.objects)
     {
-      uint8_t track_id = getOrAssignTrackId(obj.id);
+      uint8_t track_id = obj.id;
 
       ROS_DEBUG_STREAM("objectsCallback: original_id=" << obj.id
                        << ", assigned_track_id=" << static_cast<int>(track_id)
                        << ", label=" << obj.label);
 
-      kf_msgs::LidarObstacle o;
+      kf_msgs::FusionObstacle o;
       o.track_id = track_id;
-      fillFromDetectedObject(obj, o);
+      fillFromDetectedObject(obj, o); // 这里用的就是 base_link 坐标系下的 pose
       out.obstacles.push_back(o);
     }
 
-    ROS_INFO_STREAM("objectsCallback: publishing obstacles size=" 
+    ROS_INFO_STREAM("objectsCallback: publishing obstacles size="
                     << out.obstacles.size());
 
     cleanupOldTracks();
@@ -114,7 +172,7 @@ private:
   }
 
   void fillFromDetectedObject(const autoware_msgs::DetectedObject& src,
-                              kf_msgs::LidarObstacle& dst) const
+                              kf_msgs::FusionObstacle& dst) const
   {
     // 加速度、速度
     dst.a     = src.acceleration.linear.x;
@@ -174,7 +232,7 @@ private:
 
   uint8_t classifyType(const std::string& label) const
   {
-    using kf_msgs::LidarObstacle;
+    using kf_msgs::FusionObstacle;
 
     std::string lower = toLower(label);
 
@@ -183,32 +241,32 @@ private:
         lower.find("truck") != std::string::npos ||
         lower.find("bus") != std::string::npos)
     {
-      return LidarObstacle::type_veh;
+      return FusionObstacle::type_veh;
     }
 
     if (lower.find("ped") != std::string::npos ||
         lower.find("person") != std::string::npos)
     {
-      return LidarObstacle::type_ped;
+      return FusionObstacle::type_ped;
     }
 
     if (lower.find("bike") != std::string::npos ||
         lower.find("bicycle") != std::string::npos ||
         lower.find("cycl") != std::string::npos)
     {
-      return LidarObstacle::type_cyc;
+      return FusionObstacle::type_cyc;
     }
 
-    return LidarObstacle::type_unknown;
+    return FusionObstacle::type_unknown;
   }
 
   uint8_t classifyMotion(const autoware_msgs::DetectedObject& src) const
   {
-    using kf_msgs::LidarObstacle;
+    using kf_msgs::FusionObstacle;
 
     if (!src.velocity_reliable)
     {
-      return LidarObstacle::motion_type_unknown;
+      return FusionObstacle::motion_type_unknown;
     }
 
     const double vx = src.velocity.linear.x;
@@ -217,10 +275,10 @@ private:
 
     if (speed < static_speed_threshold_)
     {
-      return LidarObstacle::motion_type_static;
+      return FusionObstacle::motion_type_static;
     }
 
-    return LidarObstacle::motion_type_moving;
+    return FusionObstacle::motion_type_moving;
   }
 
   void trafficLightsCallback(const autoware_msgs::DetectedObjectArray::ConstPtr& msg)
@@ -339,49 +397,7 @@ private:
 
     // 其它有颜色的（red/green/yellow）都当作点亮
     return TrafficLight::status_on;
-  }
-
-  uint8_t getOrAssignTrackId(uint32_t original_id)
-  {
-    auto it = id_map_.find(original_id);
-    if (it != id_map_.end())
-    {
-      it->second.last_seen_frame = frame_counter_;
-      return it->second.small_id;
-    }
-
-    for (size_t i = 0; i < used_ids_.size(); ++i)
-    {
-      if (!used_ids_[i])
-      {
-        used_ids_[i] = true;
-        TrackInfo info;
-        info.small_id = static_cast<uint8_t>(i);
-        info.last_seen_frame = frame_counter_;
-        id_map_[original_id] = info;
-        return info.small_id;
-      }
-    }
-
-    auto victim_it = id_map_.begin();
-    for (auto it2 = id_map_.begin(); it2 != id_map_.end(); ++it2)
-    {
-      if (it2->second.last_seen_frame < victim_it->second.last_seen_frame)
-      {
-        victim_it = it2;
-      }
-    }
-
-    uint8_t reused_id = victim_it->second.small_id;
-    id_map_.erase(victim_it);
-
-    TrackInfo info;
-    info.small_id = reused_id;
-    info.last_seen_frame = frame_counter_;
-    id_map_[original_id] = info;
-
-    return reused_id;
-  }
+  }  
 
   void cleanupOldTracks()
   {
